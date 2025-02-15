8ee835c1dae3feee6dba1f625a3d84b79657530e
Reem-Amr7 <147060181+Reem-Amr7@users.noreply.github.com>
2025-02-15 21:32:03 +0200

    new

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,34 +1,24 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from "./Home.module.css";
 import Navbar from '../Navbar/Navbar';
 import Layout from '../Layout/Layout';
+import Leftside from './Leftside';
+import Maincontent from './Maincontent';
+import Rightside from './Rightside';
 
-=======
-import React from 'react'
-import styles from "./Home.module.css"
-import Navbar from '../Navbar/Navbar'
-import Layout from '../Layout/Layout'
-import Leftside from './Leftside'
-import Maincontent from './Maincontent'
-import Rightside from './Rightside'
->>>>>>> 02e6595784f72aaac0558b601d11f73ee4aac93c
 export default function Home() {
   const navigate = useNavigate(); // استدعاء useNavigate
 
   return (
     <div>
-<<<<<<< HEAD
-      <button onClick={() => navigate('/product-details')}>details</button>
-=======
-<div className="grid grid-cols-12 gap-4 p-4">
-      <Leftside/>
-     <Maincontent/>
-     <Rightside/>
-    </div>
->>>>>>> 02e6595784f72aaac0558b601d11f73ee4aac93c
+      {/* <button onClick={() => navigate('/product-details')}>details</button> */}
+
+      <div className="grid grid-cols-12 gap-4 p-4">
+        <div className="col-span-3"><Leftside /></div>
+        <div className="col-span-6"><Maincontent /></div>
+        <div className="col-span-3"><Rightside /></div>
+      </div>
     </div>
   );
 }
-
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,81 +1,67 @@
 import React from 'react';
 import styles from './Navbar.module.css'; 
 import { Link } from 'react-router-dom';
-import photo from "./../../assets/Screenshot 2025-02-13 210809.png"
+import photo from "./../../assets/Screenshot 2025-02-13 210809.png";
 
 export default function Navbar() {
   return (
     <>
-<header className={styles.header}>
-     
-     <div className={styles.topBar}>
-     <div className='mr-16 '><i className="fas fa-globe "></i>
-     <span className='mr-2 '> عربى</span></div>
-    <div className='mr-16 '>
-    <i class="fas fa-truck"></i>
+      <header className={styles.header}>
+        <div className={styles.topBar}>
+          <div className='mr-16'>
+            <i className="fas fa-globe"></i>
+            <span className='mr-2'>عربى</span>
+          </div>
+          <div className='mr-16'>
+            <i className="fas fa-truck"></i>
+            <span className='mr-4'>تتبع شحنتك</span>
+          </div>
+        </div>
 
-<span className='mr-4 '>تتبع شحنتك</span>
-    </div>
-     </div>
+        <nav className={styles.nav}>
+          <h1 className={styles.logo}>تراثنا</h1>
 
-     <nav className={styles.nav}>
-      
-       <h1 className={styles.logo} >تراثنا</h1>
+          <div className={styles.searchContainer}>
+            <input
+              type="text"
+              placeholder="بحث في المنشورات"
+              className={styles.searchInput}
+              dir="rtl"
+            />
+            <button className={styles.searchButton}>بحث</button>
+          </div>
 
-       <div className={styles.searchContainer}>
-         <input
-           type="text"
-           placeholder="بحث في المنشورات"
-           className={styles.searchInput}
-           dir="rtl" 
-         />
-         <button className={styles.searchButton}>بحث</button>
-       </div>
+          <div className={styles.profile}>
+            <img src={photo} alt="الصورة الشخصية" className={styles.profileImage} />
+            <span className={styles.profileName}>الاسم</span>
+          </div>
 
-    
-       <div className={styles.profile}>
-       <img src={photo} alt="الصورة الشخصية" className={styles.profileImage} />
-         <span className={styles.profileName}>الاسم</span>
-       </div>
+          <div className={styles.socialIcons}>
+            <button className={styles.socialButton}>
+              <i className="fas fa-tv"></i>
+            </button>
+            <button className={styles.socialButton}>
+              <i className="fas fa-shopping-cart"></i>
+            </button>
+            <button className={styles.socialButton}>
+              <i className="fas fa-bell"></i>
+            </button>
+            <button className={styles.socialButton}>
+              <i className="fas fa-comment-alt"></i>
+            </button>
+            <button className={styles.socialButton}>
+              <i className="fas fa-th-large"></i>
+            </button>
+          </div>
+        </nav>
+      </header>
 
-<<<<<<< HEAD
       <div className={styles.menu}>
         <Link to="/collections" className={styles.menuLink}>مجموعاتك</Link>
         <Link to="/blog" className={styles.menuLink}>مدونة</Link>
         <Link to="/shop" className={styles.menuLink}>تسوق</Link>
         <Link to="/" className={styles.menuLink}>الرئيسية</Link>
       </div>
-    </header>
-=======
-       <div className={styles.socialIcons}>
-       <button className={styles.socialButton}>
-           <i className="fas fa-tv"></i> 
-         </button>
-         <button className={styles.socialButton}>
-           <i className="fas fa-shopping-cart"></i> 
-         </button>
-         <button className={styles.socialButton}>
-           <i className="fas fa-bell"></i> 
-         </button>
-         <button className={styles.socialButton}>
-           <i className="fas fa-comment-alt"></i> 
-         </button>
-         <button className={styles.socialButton}>
-           <i className="fas fa-th-large"></i> 
-         </button>
-       </div>
-     </nav>
-
-   
-   </header>
-   <div className={styles.menu}>
-       <Link to="/collections" className={styles.menuLink}>مجموعاتك</Link>
-       <Link to="/" className={styles.menuLink}>الرئيسية</Link>
-       <Link to="/shop" className={styles.menuLink}>تسوق</Link>
-
-       <Link to="/blog" className={styles.menuLink}>مدونة</Link>
-     </div>
-   </>
->>>>>>> 02e6595784f72aaac0558b601d11f73ee4aac93c
+    </>
   );
 }
